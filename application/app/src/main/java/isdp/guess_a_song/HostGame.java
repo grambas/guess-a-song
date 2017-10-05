7690ebd7a4d60be25053435c6ccefe6b85810a66
Mindau <milius.mindaugas@yahoo.de>
2017-10-06 01:01:17 +0200

    objects migration in activities,validation

--- application/app/src/main/java/isdp/guess_a_song/HostGame.java
+++ application/app/src/main/java/isdp/guess_a_song/HostGame.java
@@ -4,17 +4,75 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+
+import isdp.guess_a_song.model.Settings;
+import isdp.guess_a_song.utils.Constants;
+import isdp.guess_a_song.utils.Validation;
 
 public class HostGame extends AppCompatActivity {
+
+    private Button btNext;
+    private RadioGroup radioGroup;
+    private int game_type;
+    private EditText guess_time;
+    private EditText songs_amount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_host_game);
+
+        btNext = (Button) findViewById(R.id.btStep1Next);
+        radioGroup = (RadioGroup) findViewById(R.id.etGameType);
+        guess_time = (EditText)findViewById(R.id.etTimeSong);
+        songs_amount = (EditText)findViewById(R.id.etNumberQuestions);
+
+        btNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HostGame.this, SelectSongs.class);
+
+                int selectedId=radioGroup.getCheckedRadioButtonId();
+                int game_type=0;
+                if(selectedId==R.id.rbGuessArtist){game_type= Constants.GAME_TYPE_ARTIST;}
+                else if(selectedId==R.id.rbGuessTitle){game_type=Constants.GAME_TYPE_TITLE;}
+
+
+                if ( checkValidation() ){
+                    intent.putExtra("game_settings", new Settings(Integer.parseInt(guess_time.getText().toString()),
+                            Integer.parseInt(songs_amount.getText().toString()), game_type));
+
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText(HostGame.this, "Form contains error", Toast.LENGTH_LONG).show();
+                }
+
+
+                //intent.putExtra("guess_time",Integer.parseInt(guess_time.getText().toString()) );
+
+
+
+            }
+        });
     }
 
     public void selectSongs(View view) {
         Intent intent = new Intent(this, SelectSongs.class);
         startActivity(intent);
     }
+
+    private boolean checkValidation() {
+        boolean ret = true;
+        if (!Validation.hasText(guess_time)) ret = false;
+        if (!Validation.hasText(songs_amount)) ret = false;
+
+        return ret;
+    }
+
 }
---
+++ application/app/src/main/java/isdp/guess_a_song/model/Settings.java
@@ -0,0 +1,118 @@
+package isdp.guess_a_song.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Created on 10/5/2017, 11:01 PM
+ */
+
+/**
+ * This is moodel Object of game settings.
+ * This class can be send to other activities with Parcelable method
+ */
+
+
+/**
+ *  Parcelling Constructors
+ */
+public class Settings implements Parcelable {
+    private int guess_time;
+    private int songs_amount;
+    private int game_type;
+
+    public static final Parcelable.Creator CREATOR = new Parcelable.Creator() {
+        public Settings createFromParcel(Parcel in) {
+            return new Settings(in);
+        }
+
+        public Settings[] newArray(int size) {
+            return new Settings[size];
+        }
+    };
+    /**
+     *  Normal Constructors
+     */
+
+    /**
+     * Default constructor with default values
+     */
+    public Settings() {
+        this.guess_time =15;
+        this.songs_amount = 10;
+        this.game_type = 1;
+    }
+
+     /**
+     * Constructor with all parameters
+     * @param guess_time
+     * @param songs_amount
+     * @param game_type
+     */
+    public Settings(int guess_time, int songs_amount, int game_type) {
+        this.guess_time = guess_time;
+        this.songs_amount = songs_amount;
+        this.game_type = game_type;
+    }
+
+    /**
+     *  Getter & Setter
+     */
+
+    public int getGuess_time() {
+        return guess_time;
+    }
+
+    public int getSongs_amount() {
+        return songs_amount;
+    }
+
+    public int getGame_type() {
+        return game_type;
+    }
+
+    public void setGuess_time(int guess_time) {
+        this.guess_time = guess_time;
+    }
+
+    public void setSongs_amount(int songs_amount) {
+        this.songs_amount = songs_amount;
+    }
+
+    public void setGame_type(int game_type) {
+        this.game_type = game_type;
+    }
+
+    /**
+     *  Parcelling part
+     */
+
+    public Settings(Parcel in){
+        this.guess_time = in.readInt();
+        this.songs_amount = in.readInt();
+        this.game_type =  in.readInt();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(this.guess_time);
+        dest.writeInt(this.songs_amount);
+        dest.writeInt(this.game_type);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "guess_time='" + guess_time + '\'' +
+                ", songs_amount='" + songs_amount + '\'' +
+                ", game_type='" + game_type + '\'' +
+                '}';
+    }
+
+
+}
