f32d27798437734c0ac715cd630b6de58306c21c
FIrstus <Firstus@web.de>
2017-10-06 16:48:54 +0200

    Updaten maxsen to the latest version

---
+++ application/app/src/main/java/isdp/guess_a_song/EditQuestion.java
@@ -0,0 +1,13 @@
+package isdp.guess_a_song;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class EditQuestion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_question);
+    }
+}
